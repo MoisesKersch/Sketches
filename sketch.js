da667511c6228c90aeb48105d395b6aa23082c00
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2018-01-02 09:53:51 -0200

    updating

--- sketch.js
+++ sketch.js
@@ -1,28 +1,5 @@
 var s;
 
-function Snake()
-{
-    createP('this is a text2');
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = .5;
-    this.yspeed = 0;
-    this.counter =  0;
-
-    this.update = function ()
-    {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
-    }
-
-    this.show = function()
-    {
-      fill(255);
-      rect(this.x, this.y, 10, 10);
-    }
-}
-
-
 function setup()
 {
     createCanvas(200,200);
